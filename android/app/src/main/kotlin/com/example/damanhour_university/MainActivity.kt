03302d76e66988d7ef0ea451026cebc46f01da67
unknown <noureenelsayd@gmail.com>
2025-03-18 07:54:11 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/damanhour_university/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.damanhour_university
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:damanhour_university/modules/home/home_screen.dart';
+import 'package:damanhour_university/shared/bloc_observer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main()  {
+ // WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+ // await CacheHelper.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+            home: HomeScreen(),
+    );
+  }
+}
